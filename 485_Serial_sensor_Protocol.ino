e7e1ab986529b79936fec30914b61aee3d54d4da
Elias Müller <elias.muller1@gmail.com>
2021-03-30 17:24:02 -0300

    'ajuste'

--- 485_Serial_sensor_Protocol.ino
+++ 485_Serial_sensor_Protocol.ino
@@ -4,13 +4,22 @@
  int pos = 0;
 
 #include <EEPROM.h>
+// Inclusao das bibliotecas
+#include <OneWire.h>
+#include <DallasTemperature.h>
+float temperature =0;
+
+const int PINO_ONEWIRE = 8; // Define pino do sensor
+OneWire oneWire(PINO_ONEWIRE); // Cria um objeto OneWire
+DallasTemperature sensor(&oneWire); // Informa a referencia da biblioteca dallas temperature para Biblioteca onewire
+DeviceAddress endereco_temp; 
 
 void setup() {
   pinMode(4, OUTPUT);
   pinMode(13, OUTPUT);
   digitalWrite(4, LOW);
   Serial.begin(9600);
-  
+  sensor.begin();
 }
 
 void serialEvent() {
@@ -45,7 +54,14 @@ void serialEvent() {
 
 void loop() {
 
-
+  sensor.requestTemperatures(); // Envia comando para realizar a conversão de temperatura
+  if (!sensor.getAddress(endereco_temp,0)) { // Encontra o endereco do sensor no barramento
+    Serial.println("SENSOR NAO CONECTADO"); // Sensor conectado, imprime mensagem de erro
+  } else {
+    temperature =  sensor.getTempC(endereco_temp);
+    
+  }
+  
   //configuração do sensor
   if (cameMessage && message[0] == "AA")
   {
@@ -58,8 +74,8 @@ void loop() {
   {
     //Configura 485 para escrita
     digitalWrite(4, HIGH);
-    //TODO colocar temperatura do sensor
-    Serial.println(message[0]+"|50");
+    //manda dado com temperatura devolta
+    Serial.println(message[0]+"|"+temperature+"|");
     //Espera até q último byte ssaia do buffer
     while ((UCSR0A & _BV (TXC0)) == 0){}
     //volta para leitura
@@ -97,7 +113,6 @@ void sensorNumberConfig() {
   {
     Serial.println("Configuração do sensor inválida");
   }
-
 
   cameMessage = false;
   //volta para leitura
